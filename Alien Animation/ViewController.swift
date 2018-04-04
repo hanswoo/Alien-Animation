f409bd7a7b3e973376998cfc1175d7587d04ca11
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-04 12:14:32 +0900

    counter

--- Alien Animation/ViewController.swift
+++ Alien Animation/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var miv: UIImageView!
+    @IBOutlet weak var ct: UILabel!
+    var counter = 1
+    var chk = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //첫번째 이미지 보이기
+        miv.image = UIImage(named: "frame1.png")
+        ct.text = "1"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBAction func ud(_ sender: Any) {
+        if counter == 5{
+            chk = false
+        }else if counter == 1{
+            chk = true
+        }
+        if chk == true{
+            counter = counter + 1
+            miv.image = UIImage(named: "frame\(counter).png")
+           ct.text = String(counter)
+        }else if chk == false{
+            counter = counter - 1
+            miv.image = UIImage(named: "frame\(counter).png")
+            ct.text = String(counter)
+        }
 }
-
+}
