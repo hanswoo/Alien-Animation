48588a4a800951a276cecbb12a9ab55b3f66d965
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-04 12:18:17 +0900

    check

--- Alien Animation/ViewController.swift
+++ Alien Animation/ViewController.swift
@@ -23,19 +23,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func ud(_ sender: Any) {
-        if counter == 5{
-            chk = false
-        }else if counter == 1{
-            chk = true
-        }
-        if chk == true{
+        if counter == 5 {
+            counter = 1
+        } else {
             counter = counter + 1
-            miv.image = UIImage(named: "frame\(counter).png")
-           ct.text = String(counter)
-        }else if chk == false{
-            counter = counter - 1
-            miv.image = UIImage(named: "frame\(counter).png")
-            ct.text = String(counter)
         }
+        miv.image = UIImage(named: "frame\(counter).png")
+    }
 }
-}
+        //if counter == 5{
+            //chk = false
+        //}else if counter == 1{
+           // chk = true
+        //}
+        //if chk == true{
+         //   counter = counter + 1
+          //  miv.image = UIImage(named: "frame\(counter).png")
+          // ct.text = String(counter)
+       // }else if chk == false{
+        //    counter = counter - 1
+         //   miv.image = UIImage(named: "frame\(counter).png")
+         //   ct.text = String(counter)
+        //}
+//}
+//}
